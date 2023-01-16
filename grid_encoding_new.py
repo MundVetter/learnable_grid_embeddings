cfd600ee30a876c473ecb9deff34e141d00f1b52
Mund Vetter <mundvetter@gmail.com>
2023-01-16 22:30:41 +0100

    non random

--- grid_encoding_new.py
+++ grid_encoding_new.py
@@ -24,16 +24,31 @@ def hexagon_encoding_new(r, r0, omega, theta):
 
     return G
 
-def grid_encoding_new(max_len, dim, factor = 10_000, seed = 2147483647):
+def grid_encoding_new(max_len, dim, factor = 10_000, seed = 2147483647, rand = False, lamda_lower = 0.28, lamda_upper = 1.0, spacing_ratio = 1.42):
     position = get_all_positions(max_len)
 
-    i = torch.linspace(0.0, 3.65, dim)
-    omega = factor * 0.28 * 1.42 ** i
 
-    generator = torch.Generator().manual_seed(seed)
+    if rand:
+        c = - math.log(lamda_lower) / math.log(lamda_upper)
+        i = torch.linspace(0.0, c, dim)
+        omega = factor * 0.28 * 1.42 ** i
+        generator = torch.Generator().manual_seed(seed)
 
-    thetas = np.pi / 3 * torch.rand(dim, generator=generator)
-    r0s = torch.rand(dim, 2, generator=generator) * torch.stack([omega, omega], dim=-1)
+        thetas = np.pi / 3 * torch.rand(dim, generator=generator)
+        r0s = torch.rand(dim, 2, generator=generator) * torch.stack([omega, omega], dim=-1)
+    else:
+        num_lambda = int(np.log(1/0.28) / np.log(spacing_ratio)) + 1
+        i = torch.arange(num_lambda)
+        num_thetas = 8
+        thetas = torch.arange(0, np.pi / 3, np.pi / 3 / num_thetas)
+        num_r0s = 2
+        x = torch.arange(0.0, 1.0, 1 / num_r0s)
+        y = torch.arange(0.0, 1.0, 1 / num_r0s)
+        omega = factor * 0.28 * 1.42 ** i
+        parms = torch.cartesian_prod(omega, thetas, x, y)
+        omega = parms[:, 0]
+        thetas = parms[:, 1]
+        r0s = parms[:, 2:] * torch.stack([omega, omega], dim=-1)
 
     G = hexagon_encoding_new(position, r0s, omega, thetas)
 
@@ -45,7 +60,7 @@ if __name__ == "__main__":
     plot_embed = False
 
     # test sum behavior
-    encodings = grid_encoding_new(28, 128, factor = 100)
+    encodings = grid_encoding_new(28, 128, factor = 40)
     if sum_test:
         values = encodings.sum(dim=-1)
         # plt.imshow(encodings[:,:,:3])
